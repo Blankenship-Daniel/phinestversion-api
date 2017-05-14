241e864314c5970570c4cff082f0274a6d552e0a
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2017-05-13 21:42:07 -0600

    add cron script to update database

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,16 @@ WORKDIR /var/www/laravel
 
 COPY . /var/www/laravel
 
-RUN apt-get update && apt-get install -y php5-sqlite && apt-get install -y php5-gd && composer install
+RUN apt-get update && apt-get install -y php5-sqlite && apt-get install -y php5-gd apt-get install -y cron && composer install
+
+# Add crontab file in the cron directory
+ADD crontab /etc/cron.d/updatedb-cron
+
+# Give execution rights on the cron job
+RUN chmod 0644 /etc/cron.d/updatedb-cron
+
+# Create the log file to be able to run tail
+RUN touch /var/log/cron.log
 
 EXPOSE 8000
 
---
+++ storage/updateDB.php
@@ -0,0 +1,90 @@
+#!/usr/bin/php
+<?php
+$db = new PDO('sqlite:database.sqlite');
+$db->setAttribute(PDO::ATTR_ERRMODE, 
+                  PDO::ERRMODE_EXCEPTION);
+
+function updateDB($db) 
+{
+    $db->exec('DELETE FROM shows');
+    $db->exec('DELETE FROM songs');
+    $db->exec('DELETE FROM venues');   
+    
+    updateShows($db);
+    updateSongs($db);
+    updateVenues($db);
+}
+
+function updateShows($db)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://phish.in/api/v1/shows.json?per_page=5000&page=1&sort_attr=date&sort_dir=desc');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+
+    $json = json_decode($output);
+   
+    foreach ($json->data as $d) {
+        $stmt = $db->prepare("INSERT INTO shows (id, date, tour_id, venue_id) VALUES (:id, :date, :tour_id, :venue_id)");
+        $stmt->bindParam(':id', $d->id);
+        $stmt->bindParam(':date', $d->date);
+        $stmt->bindParam(':tour_id', $d->tour_id);
+        $stmt->bindParam(':venue_id', $d->venue_id);
+        $stmt->execute();
+    }
+ 
+    curl_close($ch);  
+}
+
+function updateSongs($db) 
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://phish.in/api/v1/songs?per_page=5000');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+
+    $json = json_decode($output);
+   
+    foreach ($json->data as $d) {
+        $stmt = $db->prepare("INSERT INTO songs (id, title, tracks_count, slug) VALUES (:id, :title, :tracks_count, :slug)");
+        $stmt->bindParam(':id', $d->id);
+        $stmt->bindParam(':title', $d->title);
+        $stmt->bindParam(':tracks_count', $d->tracks_count);
+        $stmt->bindParam(':slug', $d->slug);
+        $stmt->execute();
+    }
+ 
+    curl_close($ch);  
+}
+
+function updateVenues($db) 
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://phish.in/api/v1/venues?per_page=5000');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+
+    $json = json_decode($output);
+   
+    foreach ($json->data as $d) {
+        $stmt = $db->prepare("INSERT INTO venues (id, name, shows_count, location, slug) VALUES (:id, :name, :shows_count, :location, :slug)");
+        $stmt->bindParam(':id', $d->id);
+        $stmt->bindParam(':name', $d->name);
+        $stmt->bindParam(':shows_count', $d->shows_count);
+        $stmt->bindParam(':location', $d->location);
+        $stmt->bindParam(':slug', $d->slug);
+        $stmt->execute();
+    }
+ 
+    curl_close($ch);    
+}
+
+try {
+    updateDB($db);
+}
+catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+$db = null;
+?>
